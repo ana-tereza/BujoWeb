efee2a5c3290307930bc49a437d51bf601e9441c
Ana Tereza <anat.oliveira7@gmail.com>
2018-12-16 19:15:47 -0300

    adicionando campos de input no login

---
+++ src/components/Button/Button.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default function Button(props) {
+  return (
+    <input
+      type="submit"
+value="Entrar"
+      //style={style}
+      //{...properties}
+    />
+  ); 
+}
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,27 @@
 import React, {Component} from 'react';
 import './index.css';
 import Label from '../Label';
+import Input from '../Input';
+import Button from '../Button';
 
 class Login extends Component {
   render() {
     return (
       <div className="center">
         <form className="pure-form pure-form-stacked">
-          <Label htmlFor="name" text="Bem vindo ao Bullet Journal!" />
+          <Label htmlFor="name" text="Bem vindo ao Bullet Journal!" /> 
+<br/><br/>
+<Input id="name" placeholder="Nome de usuário / Email" maxLength="40"/> 
+<br/><br/>
+<Input type="submit "id="password" placeholder="Senha" maxLength="40"/> 
+<br/><br/>
+<Input type="button" id="forgotpass" value="Esqueceu sua senha?"/> 
+<br/><br/>
+
+<Input type="button" id="cadastrar" value="Criar conta"/> 
+
+<Input type="button" id="logar" value="Entrar"/> 
+
         </form>
       </div>
     );
@@ -15,4 +29,10 @@ class Login extends Component {
 }
 
 export default Login;
+
+
+
+
+
+
 
