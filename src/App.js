6ab99b82997a2802d960d781ca5b2c98cce8b3b8
Ana Tereza <anat.oliveira7@gmail.com>
2018-12-16 15:50:37 -0300

    adição de background label e login, ajustes de tamanho

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import logo from './logo.svg';
 
 import './App.css';
 import Header from './components/Header';
+import Body from './components/Body';
+import Login from './components/Login';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
+        <Login />
+<Body />
       </div>
     );
   }
---
+++ src/components/Login/Login.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+import Label from '../Label';
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="center">
+        <form className="pure-form pure-form-stacked">
+          <Label htmlFor="name" text="hmmaaa?" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
+
